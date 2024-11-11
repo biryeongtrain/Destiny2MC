822c1657ef8cd5d1c8286ca2f707b56348d12239
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-11-11 23:07:20 +0900

    feat : Testing Gun Recoil System

--- src/main/java/net/biryeongtrain/destiny2mc/Destiny2mc.java
+++ src/main/java/net/biryeongtrain/destiny2mc/Destiny2mc.java
@@ -1,6 +1,13 @@
 package net.biryeongtrain.destiny2mc;
 
+import net.biryeongtrain.destiny2mc.gun.Gun;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +17,6 @@ public class Destiny2mc implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_gun"), new Gun(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "test_gun")))));
     }
 }
--- src/main/java/net/biryeongtrain/destiny2mc/gun/Gun.java
+++ src/main/java/net/biryeongtrain/destiny2mc/gun/Gun.java
@@ -1,16 +1,74 @@
 package net.biryeongtrain.destiny2mc.gun;
 
-import com.nimbusds.oauth2.sdk.id.Identifier;
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerPosition;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.network.packet.s2c.play.BundleS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRotationS2CPaket;
+import net.minecraft.network.packet.s2c.play.PositionFlag;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import xyz.nucleoid.packettweaker.PacketContext;
 
-public class Gun
+import java.util.List;
+
+public class Gun extends Item implements PolymerItem
 {
     private ElementType type;
     private Sounds Sound;
 
+    public Gun(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack getPolymerItemStack(ItemStack itemStack, TooltipType tooltipType, PacketContext context) {
+        return PolymerItem.super.getPolymerItemStack(itemStack, tooltipType, context);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(type.isAdvanced() ? Text.literal("Test!") : Text.literal("Well... this is basic."));
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, PacketContext packetContext) {
+        return Items.CROSSBOW;
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        if (user instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) user;
+            ItemStack stack = user.getStackInHand(hand);
+            recoil(player, stack);
+        }
+        return super.use(world, user, hand);
+    }
+
+    public void recoil(ServerPlayerEntity user, ItemStack stack) {
+        user.rotate(user.getYaw(), user.getPitch() - 1.5f);
+    }
 
     public static class Sounds {
         Identifier fireSound;
-        Identifier reloadSound;
-        Identifier specialSound;
+        Identifier[] reloadSound;
+        Identifier[] specialSound;
+        
+        public void playFireSound(ServerPlayerEntity user, ItemStack stack) {
+            ServerWorld world = user.getServerWorld();
+            world.playSound(null, user.getBlockPos(), SoundEvent.of(fireSound), user.getSoundCategory(), 1.0F, 1.0F);
+        }
     }
 }
